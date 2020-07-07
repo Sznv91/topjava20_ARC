4085b783595d5cad4e8b7ebdc1def702946a64e8
Mihail_Sazonov <sazonov91@gmail.com>
2020-07-07 21:54:54 +0300

    HW5 add Transactional annotation in MealRepository Interface

--- src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
@@ -1,15 +1,19 @@
 package ru.javawebinar.topjava.repository;
 
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Transactional(readOnly = true)
 public interface MealRepository {
     // null if updated meal do not belong to userId
+    @Transactional
     Meal save(Meal meal, int userId);
 
     // false if meal do not belong to userId
+    @Transactional
     boolean delete(int id, int userId);
 
     // null if meal do not belong to userId
